aabdfd67090a0db2ea6c32e3d15e6f8c6bfcc1dd
taeyeon357 <67638005+taeyeon357@users.noreply.github.com>
2023-10-12 17:45:52 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int solution(int a, int b, int n) {
+    public int solution(int a, int b, int n) {//빈 병 a병 가져다 주면 콜라 b병
         int answer = 0;
         while(n>=a) {
-        	int exchange = n/a; //바꿀 수 있는 횟수
-        	n -= (a * exchange); 
-        	n += (b * exchange);
-        	answer += b * exchange;
+        	int exchange = n/a; //현재 빈 병으로 몇 번 바꿔 먹을 수 있는지
+        	n -= (a * exchange); //빈병 소모
+        	n += (b * exchange); //새 콜라 == 빈병 추가
+        	answer += b * exchange; //바꿔 먹은 코라
         }
         return answer;
     }
