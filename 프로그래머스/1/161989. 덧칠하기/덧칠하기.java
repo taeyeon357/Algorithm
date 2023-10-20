97845d8c2a990c38daf36a311cfc1d4a183bc49c
taeyeon357 <67638005+taeyeon357@users.noreply.github.com>
2023-10-20 09:20:11 +0900

    [level 1] Title: 덧칠하기, Time: 0.87 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -3,17 +3,13 @@ class Solution {
         int answer = 0;
         
         int idx = 1;
-		while (idx <= n) {
-			for(int s : section) {
-				if(idx > s) continue;
-				else {
-					idx = s + m;
-					answer += 1;
-				}
+		for(int s : section) {
+			if(idx > s) continue;
+			else {
+				idx = s + m;
+				answer += 1;
 			}
-            break;
 		}
-        
         return answer;
     }
 }
