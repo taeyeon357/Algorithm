8d721c0ea6c20444f0e4373640dc36487fefa026
taeyeon357 <67638005+taeyeon357@users.noreply.github.com>
2024-01-05 22:51:44 +0900

    [level 1] Title: 폰켓몬, Time: 3.32 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 79.7 MB, 시간: 4.05 ms
+메모리: 77.7 MB, 시간: 3.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 16:19:24
+2024년 1월 5일 22:51:48
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,20 +4,17 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i =0; i<nums.length; i++) {
-        	hs.add(nums[i]);
+        Set<Integer> pSet = new HashSet<>();
+        for(int i=0; i<nums.length; i++) {
+        	pSet.add(nums[i]);
         }
         
-        int bringN = nums.length/2;
-        int kindsN = hs.size();
-        
-        if(kindsN >= bringN) {
-        	answer = bringN;
+        if(pSet.size() >= (nums.length/2)) {
+        	answer = nums.length/2;
         }else {
-        	answer = kindsN;
+        	answer = pSet.size();
         }
-        
+    
         return answer;
     }
 }
