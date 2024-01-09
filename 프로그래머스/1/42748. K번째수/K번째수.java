d0eb15447bf67d6923ee508f2ade0012647f5f89
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-01-09 19:10:45 +0900

    [level 1] Title: K번째수, Time: 2.24 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,19 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = {};
-                List<Integer> aList = new ArrayList<>();
+        List<Integer> answerList = new ArrayList<>();
         
-        
+        List<Integer> list = new ArrayList<>();
         for(int i=0; i<commands.length; i++) {
-        	List<Integer> list = new ArrayList<>();
-        	for(int j=commands[i][0]-1; j<=commands[i][1]-1; j++) {
+        	for(int j=commands[i][0]-1; j<commands[i][1]; j++) {
         		list.add(array[j]);
         	}
-        	Collections.sort(list); //오름차순 정렬
-        	aList.add(list.get(commands[i][2]-1));
+        	Collections.sort(list);
+        	answerList.add(list.get(commands[i][2]-1));
+        	list.clear();
         }
         
-        answer = aList.stream().mapToInt(a->a).toArray();
+        answer = answerList.stream().mapToInt(a->a).toArray();
         return answer;
     }
 }
