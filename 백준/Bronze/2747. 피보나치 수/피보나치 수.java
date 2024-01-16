91855455ad882962e1dcb198ad14a7abf924e6b1
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-01-16 19:51:36 +0900

    [Bronze II] Title: 피보나치 수, Time: 200 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<n+1; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+		System.out.println(dp[n]);
+	}
+}
