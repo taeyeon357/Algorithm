b4e14eac2d22fe46c50525816a3bc3e73e19851e
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-05-01 23:23:04 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -2,8 +2,7 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[2];
         for(int i=0; i<num_list.length; i++){
-            if(num_list[i] %2 ==0) answer[0] += 1;
-            else answer[1] += 1;
+            answer[num_list[i]%2]++;
         }
         return answer;
     }
