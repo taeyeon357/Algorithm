f987d07a8915ca9a3ec974168b259acee39f1d4c
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-09 23:05:34 +0900

    [Bronze III] Title: 공, Time: 128 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.java
+++ 백준/Bronze/1547. 공/공.java
@@ -1,22 +1,25 @@
 import java.util.*;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         boolean[] cup = new boolean[4];
         cup[1] = true;
         
-        int n = sc.nextInt();
+        int n = Integer.parseInt(st.nextToken());
         int x, y;
         for(int i=0; i<n; i++){
-            x = sc.nextInt();
-            y = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
             
-            if(cup[x]){
+            if(cup[x]) {
                 cup[y] = true;
                 cup[x] = false;
             }
-            else if(cup[y]){
+            else if(cup[y]) {
                 cup[x] = true;
                 cup[y] = false;
             }   
