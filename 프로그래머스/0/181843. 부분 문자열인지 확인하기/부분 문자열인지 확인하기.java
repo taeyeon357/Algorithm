3e5ff3ba9c5113bbca41d4034449fd69b4ec7442
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-11 15:10:08 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.02 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.java
@@ -1,15 +1,5 @@
 class Solution {
     public int solution(String my_string, String target) {
-        int tLen = target.length();
-        int sLen = my_string.length();
-        
-        int start = 0;
-        int end = tLen;
-        while(end <= sLen){
-            if(target.equals(my_string.substring(start,end))) return 1;
-            start++;
-            end++;
-        }
-        return 0;
+        return my_string.indexOf(target) > -1 ? 1 : 0;
     }
 }
