e8601b4d25475c1d824e8a917b06edf1c091399d
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-14 23:36:27 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.07 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        myString = myString.replace("A", "b");
-        myString = myString.replace("B", "a");
-        myString = myString.toUpperCase();
-       
+        myString = myString.replace("A", "b").replace("B", "a").toUpperCase(); 
         return myString.indexOf(pat) > -1 ? 1 : 0;
     }
 }
