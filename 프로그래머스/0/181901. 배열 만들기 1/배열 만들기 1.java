9d52e2159d198a633aaeacdb3dc37c6443841665
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-15 22:48:18 +0900

    [level 0] Title: 배열 만들기 1, Time: 14.25 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,14 +1,7 @@
-import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int n, int k) {
-        List<Integer> list = new ArrayList<>();
-        
-        int mul = k;
-        while(mul <= n){
-            list.add(mul);
-            mul += k;
-        }
-        return list.stream().mapToInt(i->i).toArray();
+        return IntStream.rangeClosed(1, n).filter(i -> i % k == 0).toArray();
     }
 }
