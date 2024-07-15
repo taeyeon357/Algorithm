807dfaaa72be6821a515e6d63eee86b6bed5773c
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-15 23:28:17 +0900

    [level 0] Title: 마지막 두 원소, Time: 4.65 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -3,7 +3,9 @@ import java.util.stream.Collectors;
 class Solution {
     public int[] solution(int[] num_list) {
         int len = num_list.length;
+        
         List<Integer> list = Arrays.stream(num_list).boxed().collect(Collectors.toList());
+        
         if(num_list[len-1] > num_list[len-2])  list.add(num_list[len-1] - num_list[len-2]);
         else list.add(num_list[len-1]*2);
         
