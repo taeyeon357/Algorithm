52bc1b363e001c58b890bafbf95cbddbcd4794e9
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-16 00:24:54 +0900

    [level 0] Title: 접미사 배열, Time: 0.67 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -4,7 +4,7 @@ class Solution {
     public String[] solution(String my_string) {
         String[] answer = new String[my_string.length()];
         for(int i=0; i<my_string.length(); i++){
-            answer[i] = my_string.substring(i, my_string.length());
+            answer[i] = my_string.substring(i);
         }
         Arrays.sort(answer);
         return answer;
