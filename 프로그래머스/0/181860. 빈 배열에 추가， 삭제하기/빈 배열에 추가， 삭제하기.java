65a5293644398c94c0d9c8c29629ed77b254c200
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-16 23:17:51 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 23.37 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, boolean[] flag) {
+        //문자열 형태로 원소를 추가 및 제거
         String str = "";
-        
         for(int i=0; i<flag.length; i++){
             if(flag[i]){
                 str += (""+arr[i]).repeat(arr[i]*2);
@@ -13,13 +13,16 @@ class Solution {
             }
         }
 
+        //문자열을 각 자리별로 split해서 int배열에 저장
         String[] strArr = str.split("");
         int[] ans = new int[strArr.length];
         for(int i=0; i<ans.length; i++){
             ans[i] = Integer.parseInt(strArr[i]);
         }
+        /* 스트림 활용 문자열 -> 정수 배열
         
+        Stream.of(str.split("")).mapToInt(Integer::parseInt).toArray();
+        */
         return ans;
-            
     }
 }
