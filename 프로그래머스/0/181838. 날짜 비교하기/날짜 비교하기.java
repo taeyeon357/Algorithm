e3d3e50b9fd16d9ed4e6644dca83c3a5dff45c60
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-16 22:56:25 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.08 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,16 +1,10 @@
+import java.time.LocalDate;
+
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        int answer = 0;
-        
-        if(date1[0] < date2[0])
-            answer = 1;
-        else if(date1[0] == date2[0]){
-            if(date1[1] < date2[1])
-                answer = 1;
-            else if(date1[1] == date2[1] && date1[2] < date2[2])
-                answer = 1;
-        }
-                                                
-        return answer;
+        LocalDate d1 = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate d2 = LocalDate.of(date2[0], date2[1], date2[2]);
+                              
+        return d1.isBefore(d2) ? 1 : 0;
     }
 }
