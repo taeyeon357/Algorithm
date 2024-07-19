223c249d7c88c9b014899611643eb5b7587c9e4e
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-19 22:09:36 +0900

    [level 0] Title: 배열 만들기 4, Time: 62.98 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        Stack<Integer> stk = new Stack<>();
+        Stack<Integer> stk = new Stack<>(); 
         
         int i=0;
         while(i<arr.length){
             if(stk.empty()){
                 stk.push(arr[i++]);
-            }else if(!stk.empty() && stk.peek() < arr[i]){
+            }else if(stk.peek() < arr[i]){
                 stk.push(arr[i++]);
-            }else if(!stk.empty() && stk.peek() >= arr[i]){
+            }else{
                 stk.pop();
             }
         }
+        
         return stk.stream().mapToInt(a->a).toArray();
     }
 }
