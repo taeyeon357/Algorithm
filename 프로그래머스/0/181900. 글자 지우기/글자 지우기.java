aa0fbeb1bb2ff8615676b413977d9bf933b1e427
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-19 20:32:06 +0900

    [level 0] Title: 글자 지우기, Time: 0.64 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -2,16 +2,10 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string, int[] indices) {
-        String answer = "";
-        Arrays.sort(indices);
-        int idx = 0;
-        for(int i=0; i<my_string.length(); i++){
-            if(idx < indices.length && indices[idx] ==  i){
-                idx++;
-                continue;
-            }
-            answer += my_string.charAt(i);
+        String[] str = my_string.split("");
+        for(int i : indices){
+            str[i] = "";
         }
-        return answer;
+        return String.join("", str);
     }
 }
