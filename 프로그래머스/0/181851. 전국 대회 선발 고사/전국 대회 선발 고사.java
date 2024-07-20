52f96cc7764527f110c3c58f16a471f612f00673
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-07-20 22:04:37 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 1.45 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -2,19 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int sum = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b)->rank[a] - rank[b]);
         for(int i=0; i<rank.length; i++){
-            if(attendance[i]){
-                map.put(rank[i], i);
-            }
+            if(attendance[i])
+                queue.add(i);
         }
         
-        List<Integer> key = new ArrayList<>(map.keySet());
-        Collections.sort(key);
-        sum += (map.get(key.get(0))*10000);
-        sum += (map.get(key.get(1))*100);
-        sum += (map.get(key.get(2)));        
-        return sum;
+        return queue.poll() * 10000 + queue.poll() * 100 + queue.poll();
     }
 }
