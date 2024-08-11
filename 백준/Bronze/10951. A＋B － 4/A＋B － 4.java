f8a33b6ab1e1b89317f0b123753796bbff786007
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-08-11 17:43:07 +0900

    [Bronze V] Title: A+B - 4, Time: 176 ms, Memory: 17796 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -4,16 +4,16 @@ public class Main{
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
         int a, b;
-        while(true){
-            try{
-                a = sc.nextInt();
-                b = sc.nextInt();
-            }catch(Exception e){
-                break;
-            }
-            
+      
+        StringTokenizer st;
+        while(sc.hasNext()){
+            st = new StringTokenizer(sc.nextLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+
             System.out.println(a+b);
         }
+    
         sc.close();
     }
 }
