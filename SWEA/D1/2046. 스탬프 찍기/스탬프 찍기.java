d51a4c2c575cbda3ebb7b95b3f17f18fe174ea76
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-11-14 23:35:01 +0900

    [D1] Title: 스탬프 찍기, Time: 102 ms, Memory: 18,604 KB -BaekjoonHub

--- SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -1,17 +1,16 @@
-
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.util.*;
+import java.io.*;
 
 
 class Solution
 {
-	public static void main(String args[]) throws Exception
+	public static void main(String args[]) throws IOException
 	{
-		Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=0; i<n; i++){
-       		System.out.print('#');
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) sb.append("#");
         
+        System.out.println(sb);
 	}
 }
