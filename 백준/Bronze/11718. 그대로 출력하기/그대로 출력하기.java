b4b51b9c8db1724650b58dbcfd3e9dfe34a385a9
강태연 <67638005+taeyeon357@users.noreply.github.com>
2024-11-21 20:34:28 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 192 ms, Memory: 17880 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+        sc.close();
+    }
+}
